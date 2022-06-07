01b0caaac7fed1b03c941ab265dde805fa22b002
parthvik <66945298+parthvik@users.noreply.github.com>
2022-06-07 16:03:07 +0530

    ejs connected

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,5 +2,6 @@
 //     return res.end('<h1>Express is up for face_book</h1>')
 // }
 module.exports.home = function(req,res) {
-    return res.send('<h1>Express is up for face_book</h1>')
+    return res.render('home', {title: 'Home'});
+    
 }
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,18 @@ const router= express.Router();
 const homeController= require('../controllers/home_controller');
 console.log("router loaded");
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+// router.use('/posts', require('./posts'));
+// router.use('/comments', require('./comments'));
+// router.use('/likes', require('./likes'));
+// router.use('/follows', require('./follows'));
+// router.use('/notifications', require('./notifications'));
+// router.use('/messages', require('./messages'));
+// router.use('/search', require('./search'));
+// router.use('/chat', require('./chat'));
+// router.use('/chat/messages', require('./chat_messages'));
+// router.use('/chat/messages/:id', require('./chat_messages'));
+
+
 
 module.exports =router;
