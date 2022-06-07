32d77d2ad6f42c94c12f2aaec36248e5f4d0085b
parthvik <66945298+parthvik@users.noreply.github.com>
2022-06-07 15:42:20 +0530

    second commit

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+// module.exports.home = function(req,res) {
+//     return res.end('<h1>Express is up for face_book</h1>')
+// }
+module.exports.home = function(req,res) {
+    return res.send('<h1>Express is up for face_book</h1>')
+}
